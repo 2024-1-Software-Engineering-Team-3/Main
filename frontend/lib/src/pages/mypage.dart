51dbcf742b5aca060fa326b6a0b5f1fc6ecae436
Guho-Kim <weeeeek2@naver.com>
2024-04-28 22:43:27 +0900

    front: Add bottom navigator

---
+++ frontend/lib/src/pages/mypage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Page'),
+      ),
+      body: Center(
+        child: Text('My Page'),
+      ),
+    );
+  }
+}
