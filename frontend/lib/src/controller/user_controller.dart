8d3cb827c01611878019877bba17645f1a737ad8
Guho-Kim <weeeeek2@naver.com>
2024-05-31 07:41:08 +0900

    path_provider 추가 설치

--- frontend/lib/src/controller/user_controller.dart
+++ frontend/lib/src/controller/user_controller.dart
@@ -5,11 +5,14 @@ class UserController extends GetxController {
   var shares = 0.obs;
   var reply = 0.obs;
   var likes = 0.obs;
+  var point = 0.obs;
 
-  void setUser(String user, int userShares, int userReply, int userLikes) {
+  void setUser(String user, int userShares, int userReply, int userLikes,
+      int userPoint) {
     username.value = user;
     shares.value = userShares;
     reply.value = userReply;
     likes.value = userLikes;
+    point.value = userPoint;
   }
 }
